42ca7cdbc36282aacfbdaa795c4798b9f36da58b
Jay Vyas <j3vyas@uwaterloo.ca>
2016-07-14 17:32:54 -0400

    Main code complete, need to add in pixel to irl movement code + added build command for linux

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # FYDP51
+
+#Build process = g++ detection_with_images.cpp -o detect `pkg-config --cflags --libs opencv` -lsqlite3 -std=c++11
--- detection_with_images.cpp
+++ detection_with_images.cpp
@@ -8,9 +8,110 @@
 #include <stdlib.h>
 #include <ctime>
 #include <string.h>
+#include <unistd.h>
 
 using namespace std;
 using namespace cv;
+
+vector<Point> processImage(Mat bgr_image, int low1, int low2, int low3, int high1, int high2, int high3){
+	// Show the original images
+	namedWindow("Original Image", WINDOW_NORMAL);
+	imshow("Original Image", bgr_image);
+
+	Mat orig_image = bgr_image.clone();
+
+	// Convert input image to HSV
+	Mat hsv_image;
+	cvtColor(bgr_image, hsv_image, COLOR_BGR2HSV);
+
+	// Threshold the HSV image
+	Mat hue_range;
+	inRange(hsv_image, Scalar(low1, low2, low3), Scalar(high1, high2, high3), hue_range);
+	
+	// Combine the above two images
+	Mat hue_image = hue_range.clone();
+
+	GaussianBlur(hue_image, hue_image, Size(5, 5), 2, 2);
+
+	// Show the filtered images
+	namedWindow("Threshold Image", WINDOW_NORMAL);
+	imshow("Threshold Image", hue_image);
+
+	vector<vector<Point> > contours;
+	vector<Vec4i> hierarchy;
+	findContours(hue_image, contours, CV_RETR_EXTERNAL, CV_CHAIN_APPROX_SIMPLE);
+
+	//draw contours
+	Mat drawing = Mat::zeros(hue_image.size(), CV_8UC3);
+	Mat contourDrawing = Mat::zeros(hue_image.size(), CV_8UC3);;
+	drawContours(contourDrawing, contours, -1, Scalar(255, 255, 255));
+
+	//detect centre points
+	vector<RotatedRect> boundingBox;
+	vector<Point> centrePoints;
+	for (int i = 0; i < contours.size(); i++){
+		RotatedRect rect = minAreaRect(contours[i]);
+		Point2f corners[4];
+		rect.points(corners);
+		bool isDetected = true;
+		Point2f prevCorner;
+		prevCorner = corners[0];
+
+		for (int j = 1; j < 4; j++){
+			if (corners[j] == prevCorner)
+				isDetected = false;
+		}
+		if (isDetected && (contourArea(contours[i]) >= 1500)){
+			boundingBox.push_back(rect);
+			centrePoints.push_back(rect.center);
+		}
+	}
+	// draw the rotated rect and centre point
+	for (int i = 0; i < boundingBox.size(); i++){
+		Point2f corners[4];
+		boundingBox[i].points(corners);
+		line(drawing, corners[0], corners[1], Scalar(255, 255, 255), 2);
+		line(drawing, corners[1], corners[2], Scalar(255, 255, 255), 2);
+		line(drawing, corners[2], corners[3], Scalar(255, 255, 255), 2);
+		line(drawing, corners[3], corners[0], Scalar(255, 255, 255), 2);
+		circle(drawing, centrePoints[i], 3, Scalar(255, 255, 255), -1);
+	}
+
+	// display on named windows
+	namedWindow("origFinal", WINDOW_NORMAL);
+	imshow("origFinal", drawing);
+	waitKey(1);
+	return centrePoints;	
+}
+
+void sqlStatement(time_t timeIn, int sessionID, char* query){
+	char sql[100] = "INSERT INTO information VALUES (";
+	char timeBuffer[80];
+	struct tm tstruct = *localtime(&timeIn);
+	strftime(timeBuffer, sizeof(timeBuffer), "%X", &tstruct);
+	char const *tmpSession = to_string(sessionID).c_str();
+	strcat(sql,tmpSession);
+	strcat(sql, ", '");
+	strcat(sql,timeBuffer);
+	time_t timeOut = time(0);
+	tstruct = *localtime(&timeOut);
+	strftime(timeBuffer, sizeof(timeBuffer), "%X", &tstruct);
+	strcat(sql, "', '");
+	strcat(sql, timeBuffer);
+	strcat(sql, "', ");
+	int timeDiff = difftime(timeOut,timeIn);
+	char const *timeDif = std::to_string(timeDiff).c_str();
+	strcat(sql, timeDif);
+	strcat(sql, ", '");
+	strftime(timeBuffer, sizeof(timeBuffer), "%Y-%m-%d", &tstruct);
+	strcat(sql, timeBuffer);
+	strcat(sql, "');");
+	cout << "SQL statement:\n" << sql << "\n";
+	for (int i = 0; i < 100; ++i){
+		query[i] = sql[i];
+	}
+	return;
+}
 
 int main(int argc, char **argv) {
 	//Database variables setup
@@ -18,85 +119,42 @@ int main(int argc, char **argv) {
 	char *error;
 	time_t timeIn;
 	int sessionID = 1;
-	//char *sql = "INSERT INTO information VALUES (1, '17:20:00', '17:45:00', 25, '2016-07-05')";
+	//Connect to database, exit on failure
 	int rc = sqlite3_open("SmartChair.db", &db);
 	if (rc){
 		cout << "error opening file";
 		sqlite3_close(db);
 		return 0;
 	}
-	//rc = sqlite3_exec(db,sql,NULL,0,&error);
 
+	//Connect to webcam for live feed input
+	Mat origImg;
 	VideoCapture cap(0);
 	if (!cap.isOpened()){
 		cout << "Did not open camera\n";
 		return -1;
 	}
 
+	//Center points for origin, used to return to original location
+	vector<Point> baseCenterPoints_RED;
+	vector<Point> baseCentrePoints_GREEN;
+
+	//Setup origin (and remove noise by reading first few inputs from camera)
+	for(int i = 0; i < 50; ++i){
+		cap >> origImg;
+	}
+
+	//obtain center points for red objects in image
+	baseCenterPoints_RED = processImage(origImg,160,100,100,179,255,255);
+	baseCentrePoints_GREEN = processImage(origImg,45,100,100,75,255,255);
+	
 	bool sitting = false;
-	Mat bgr_image;
 	while (1) {
-		cap >> bgr_image;
+		cap >> origImg;
 
-		// Show the original images
-		namedWindow("Original image", WINDOW_NORMAL);
-		imshow("Original image", bgr_image);
-
-		Mat orig_image = bgr_image.clone();
-	
-		// Convert input image to HSV
-		Mat hsv_image;
-		cvtColor(bgr_image, hsv_image, COLOR_BGR2HSV);
-	
-		// Threshold the HSV image
-		Mat red_hue_range;
-		inRange(hsv_image, Scalar(160, 50, 50), Scalar(255, 50, 50), red_hue_range);
-	
-		// Combine the above two images
-		Mat red_hue_image = red_hue_range.clone();
-		imwrite("filteredImg.jpg", red_hue_image);
-	
-		GaussianBlur(red_hue_image, red_hue_image, Size(5, 5), 2, 2);
-	
-		// Show the filtered images
-		namedWindow("Threshold image", WINDOW_NORMAL);
-		imshow("Threshold image", red_hue_image);
-		imwrite("cleanedImg.jpg",red_hue_image);
-	
-		vector<vector<Point> > contours;
-		vector<Vec4i> hierarchy;
-		findContours(red_hue_image, contours, CV_RETR_EXTERNAL, CV_CHAIN_APPROX_SIMPLE);
-	
-		//draw contours
-		Mat drawing = Mat::zeros(red_hue_image.size(), CV_8UC3);
-		Mat contourDrawing = Mat::zeros(red_hue_image.size(), CV_8UC3);;
-		drawContours(contourDrawing, contours, -1, Scalar(255, 255, 255));
-		imwrite("contourImg.jpg", contourDrawing);
-	
-		//detect centre points
-		vector<RotatedRect> boundingBox;
-		vector<Point> centrePoints;
-		for (int i = 0; i < contours.size(); i++){
-	
-			RotatedRect rect = minAreaRect(contours[i]);
-			Point2f corners[4];
-			rect.points(corners);
-			bool isDetected = true;
-			Point2f prevCorner;
-			prevCorner = corners[0];
-	
-			for (int j = 1; j < 4; j++){
-				if (corners[j] == prevCorner)
-					isDetected = false;
-			}
-			if (isDetected && (contourArea(contours[i]) >= 1000)){
-				boundingBox.push_back(rect);
-				centrePoints.push_back(rect.center);
-			}
-		}
-		
+		vector<Point> sittingCenterPoints_YELLOW = processImage(origImg,21,100,100,35,255,255); //
 		//Check to see if someone is sitting on chair (missing one centerpoints of contour)
-		if (centrePoints.size() < 5){
+		if (sittingCenterPoints_YELLOW.size() < 1){
 			if (!sitting){
 				timeIn = time(0);
 				sitting = true;
@@ -104,53 +162,34 @@ int main(int argc, char **argv) {
 		}
 		else{
 			if (sitting){
-				char sql[100] = "INSERT INTO information VALUES (";
-				char timeBuffer[80];
-				struct tm tstruct = *localtime(&timeIn);
-				strftime(timeBuffer, sizeof(timeBuffer), "%X", &tstruct);
-				char const *tmpSession = to_string(sessionID).c_str();
-				strcat(sql,tmpSession);
-				sessionID++;
-				strcat(sql, ", '");
-				strcat(sql,timeBuffer);
-				time_t timeOut = time(0);
-				tstruct = *localtime(&timeOut);
-				strftime(timeBuffer, sizeof(timeBuffer), "%X", &tstruct);
-				strcat(sql, "', '");
-				strcat(sql, timeBuffer);
-				strcat(sql, "', ");
-				int timeDiff = difftime(timeOut,timeIn);
-				char const *timeDif = std::to_string(timeDiff).c_str();
-				strcat(sql, timeDif);
-				strcat(sql, ", '");
-				strftime(timeBuffer, sizeof(timeBuffer), "%Y-%m-%d", &tstruct);
-				strcat(sql, timeBuffer);
-				strcat(sql, "');");
-				sitting = false;
-				cout << "SQL statement:\n" << sql << "\n";
+				char sql[100];
+				sqlStatement(timeIn, sessionID, sql);
+				rc = sqlite3_exec(db,sql,NULL,0,&error);
+				if( rc != SQLITE_OK ){
+			    	cout << "error executing sqlite3_exec\n";
+			    }
+			    sessionID++;
+			    sitting = false;
+			    //user has gotten up
+			    //sleep for 2 seconds, then move back back to origin
+			    sleep(2);
+			    for (int wipe = 0; wipe < 10; ++wipe){
+			    	cap >> origImg;
+			    }
+			    cap >> origImg;
+			    vector<Point> newCenterPoints_RED = processImage(origImg,160,100,100,179,255,255);
+			    vector<Point> newCenterPoints_GREEN = processImage(origImg,45,100,100,90,255,255);
+			    //do processing with respect to base center points and new center points
+			    /*
+			    bool notAtOrigin = true;
+			    while (notAtOrigin){
+					//move chair x,y directions to origin
+					//get new center points for red/green
+					//do math and repeat till at origin (+/- few pixels of error)
+			    }
+			    */
 			}		
 		}
-		for (int i = 0; i < centrePoints.size(); ++i){
-			cout << centrePoints[i] << " ";
-		}
-		cout << "\n";
-		// draw the rotated rect and centre point
-		for (int i = 0; i < boundingBox.size(); i++){
-			Point2f corners[4];
-			boundingBox[i].points(corners);
-			line(drawing, corners[0], corners[1], Scalar(255, 255, 255), 2);
-			line(drawing, corners[1], corners[2], Scalar(255, 255, 255), 2);
-			line(drawing, corners[2], corners[3], Scalar(255, 255, 255), 2);
-			line(drawing, corners[3], corners[0], Scalar(255, 255, 255), 2);
-			circle(drawing, centrePoints[i], 3, Scalar(255, 255, 255), -1);
-		}
-	
-		// display on named windows
-		namedWindow("drawing", WINDOW_NORMAL);
-		imshow("drawing", drawing);
-		imwrite("resultImg.jpg", drawing);
-	
-		waitKey(200);
 	}
 	sqlite3_close(db);
 	return 0;
